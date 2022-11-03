118293b5c7fcf02498794b9f85f709b624399e86
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2022-11-03 12:25:14 +0530

    finalize the new Member

--- js/manage-members.js
+++ js/manage-members.js
@@ -2,59 +2,59 @@ const pageSize =5;
 let page =3;
 getAllMembers();
 function getAllMembers(query=`${$('#txt-search').val()}`){
-    /* Step 1 --->Initiate a XMLHTTP request*/
+    /* Step 1 --->Initiate a XMLHTTP request*/ //this is called XHR 
 
-const http = new XMLHttpRequest();
+    const http = new XMLHttpRequest();
 
 
 
-/* step 2 ---> set an event listner o detect state change*/
+    /* step 2 ---> set an event listner to detect state change*/
 
-http.addEventListener('readystatechange',()=>{
-    // console.log(http.readyState);
-    /* can identify status using readystate instance property */
-    if(http.readyState===http.DONE){
-        if(http.status===200){
-            const totalMembers =+http.getResponseHeader('X-Total-Count');
-            initpagination(totalMembers);
+    http.addEventListener('readystatechange',()=>{
+        // console.log(http.readyState);
+        /* can identify status using readystate instance property */
+        if(http.readyState===http.DONE){  //reaady state changes 4-done ,3-loading 2-header recieved 1-opened
+            if(http.status===200){  //check the status code of the response
+                const totalMembers =+http.getResponseHeader('X-Total-Count');  //getting header data for paginated results
+                initpagination(totalMembers);
 
-            const members = JSON.parse(http.responseText);
-            // console.log($('#loader'))
-            // $('#loader').hide();
-            if(members.length===0){
-                $('#tbl-members').addClass('empty');
+                const members = JSON.parse(http.responseText);
+                $('#loader').hide();
+                if(members.length===0){
+                    $('#tbl-members').addClass('empty');
+                }else{
+                    $('#tbl-members').removeClass('empty');
+                }
+                $('#tbl-members tbody tr').remove();
+                members.forEach(member => {
+                    const rowHtml=`
+                        <tr tabindex="0">
+                            <td>${member.id}</td>
+                            <td>${member.name}</td>
+                            <td>${member.address}</td>
+                            <td>${member.contact}</td>
+                        </tr>
+                    `;
+                $('#tbl-members tbody').append(rowHtml);
+                });
             }else{
-                $('#tbl-members').removeClass('empty');
+                // $('#toast').show();
+                showToast('Failed to load the Members','warning');
             }
-            $('#tbl-members tbody tr').remove();
-            members.forEach(member => {
-                const rowHtml=`
-                    <tr tabindex="0">
-                        <td>${member.id}</td>
-                        <td>${member.name}</td>
-                        <td>${member.address}</td>
-                        <td>${member.contact}</td>
-                    </tr>
-                `;
-            $('#tbl-members tbody').append(rowHtml);
-            });
-        }else{
-            $('#toast').show();
         }
-    }
 
-});
+    });
 
-/* step-3 -->Open the requst */
+    /* step-3 -->Open the requst */
 
-http.open('GET',`http://localhost:8080/lms/api/members?size=${pageSize}&page=${page}&q=${query}`,true);  /* here true means assynchronous */
+    http.open('GET',`http://localhost:8080/lms/api/members?size=${pageSize}&page=${page}&q=${query}`,true);  /* here true means assynchronous */
 
-/* step-4 -->set additionl information for the request */
+    /* step-4 -->set additionl information for the request */
 
 
-/* step-5 -> Send the request */
+    /* step-5 -> Send the request */
 
-http.send();
+    http.send();
 }
 
 
@@ -122,4 +122,144 @@ $(document).keydown((eventData)=>{
     if(eventData.ctrlKey && eventData.key ==='/'){
         $('#txt-search').focus();
     }
+});
+
+$('#btn-new-member').click(()=>{
+    const frmMemberDetail = new bootstrap.Modal(document.getElementById('frm-member-detail'));
+    $('#frm-member-detail').addClass('new')
+    .on('shown.bs.modal',()=>{
+        $('#txt-name').focus();
+    });
+    frmMemberDetail.show();
+
+});
+
+$('#frm-member-detail form').submit((eventData)=>{
+    eventData.preventDefault();
+    $('#btn-save').click();
+    
+});
+
+$('#btn-save').click(async()=>{
+    const name =$('#txt-name').val();
+    const address =$('#txt-address').val();
+    const contact =$('#txt-contact').val();
+    let validated =true;
+
+    $('#txt-name,#txt-address,#txt-contact').removeClass('is-invalid');
+
+    if(!/^[A-Za-z ]+$/.test(name)){
+        $('#txt-name').addClass('is-invalid').select().focus();
+        validated=false;
+    }
+
+    if(!/^[A-Za-z0-9|,.:;#/\ -]+$/.test(address)){
+        $('#txt-address').addClass('is-invalid').select().focus();
+        validated=false;
+    }
+    if(!/^\d{3}-\d{7}$/.test(contact)){
+        $('#txt-contact').addClass('is-invalid').select().focus();
+        validated=false;
+
+    }
+
+    if(!validated) return;
+
+    try{
+        $('#overlay').removeClass('d-none');
+        const {id}=await saveMember();
+        $('#overlay').addClass('d-none');
+        showToast(`Successfully Saved Successfully with the ID:${id}`,'success');
+        $('#txt-name,#txt-address,#txt-contact').val("");
+        $('#txt-name').focus();
+    }catch(e){
+        $('#overlay').addClass('d-none');
+        showToast('Failed to save the member,try again!','error');
+        $('#txt-name').focus();
+    }
+});
+
+
+
+function saveMember(){
+    return new Promise((resolve,reject)=>{  //if  promise completed ->resolve funtion works if not reject funtion works
+
+        const xhr =new XMLHttpRequest();
+
+        xhr.addEventListener('readystatechange',()=>{
+            console.log(xhr.readyState,XMLHttpRequest.DONE)
+            if(xhr.readyState===xhr.DONE){
+                if(xhr.status===201){
+                    resolve(JSON.parse(xhr.responseText));
+                }else{
+                    reject();
+                }
+            }
+        });
+
+        xhr.open('POST','http://localhost:8080/lms/api/members',true);
+        xhr.setRequestHeader('Content-Type','application/json');
+
+        const member ={
+            name:$('#txt-name').val(),
+            address:$('#txt-address').val(),
+            contact:$('#txt-contact').val()
+        }
+        xhr.send(JSON.stringify(member));
+
+    });
+}
+// doSomething();
+// async function doSomething(){
+//     try{
+//         await saveMember();
+//         console.log('promise eka una widiyatama wada karanawa');
+
+//     }catch(e){
+//         console.log('Promise eka kalea');
+
+//     }
+// }
+
+// const promise = saveMember();
+
+// promise.then(()=>{
+//     console.log('Kiwwa wagema kala..!')
+// }).catch(()=>{
+//     console.log('promise eka kale...!')
+// });
+
+// promise.then(()=>{
+//     console.log('Kiwwa wagema kala 1..!')
+// }).catch(()=>{
+//     console.log('promise eka kale 2...!')
+// }).finally(()=>{
+//     console.log('wade unath nathath finally wada');
+// });
+
+
+
+
+function showToast(msg,msgType='warning'){
+    $('#toast').removeClass('text-bg-warning')
+    .removeClass('text-bg-primary')
+    .removeClass('text-bg-error')
+    .removeClass('text-bg-success');
+
+    if(msgType==='success'){
+        console.log('success working')
+        $('#toast').addClass('text-bg-success');
+    }else if(msgType==='error'){
+        $('#toast').addClass('text-bg-error');
+    }else if(msgType==='info'){
+        $('#toast').addClass('text-bg-primary');
+    }else{
+        $('#toast').addClass('text-bg-warning');
+    }      
+    $('#toast .toast-body').text(msg);
+    $('#toast').toast('show');
+}
+
+$('#frm-member-detail').on('hidden.bs.modal',()=>{
+    getAllMembers();
 });
